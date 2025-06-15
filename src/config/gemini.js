c6d86c467062b41de18e5de526f96c2f3aa90e5c
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2025-06-15 16:08:41 +0530

    trying to use vercel servless function

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,32 +1,10 @@
-import {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } from "@google/generative-ai"
-  
-  const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash-exp",
+import axios from "axios";
+
+async function run(prompt) {
+  const response = await axios.post("https://ai-sharthi.vercel.app/api/ai", {
+    prompt,
   });
-  
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 40,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-  };
-  
-  async function run(prompt) {
-    const chatSession = model.startChat({
-      generationConfig,
-      history: [
-      ],
-    });
-  
-    const result = await chatSession.sendMessage(prompt);
-    const response = result.response;
-    return response.text();
-  }
-  
-  export default run;
+  return response.data;
+}
+
+export default run;
