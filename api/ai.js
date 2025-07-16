f245bdbc162afaac95b8b233c22dbeabdd3ec3ba
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2025-07-16 11:16:39 +0530

    bug fixes

--- api/ai.js
+++ api/ai.js
@@ -1,20 +1,19 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
-const genAI = new GoogleGenerativeAI(process.env.VITE_GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash-exp",
-});
-
-const generationConfig = {
-  temperature: 1,
-  topP: 0.95,
-  topK: 40,
-  maxOutputTokens: 8192,
-  responseMimeType: "text/plain",
-};
 export default async function handler(request, response) {
+  const genAI = new GoogleGenerativeAI(process.env.VITE_GEMINI_API_KEY);
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash-exp",
+  });
 
-  const {prompt} = request.body;
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+
+  const { prompt } = request.body;
   async function run(prompt) {
     const chatSession = model.startChat({
       generationConfig,
