d11d5f29a5f9ee472054c890a938de5fe2e4c862
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2025-09-29 22:15:06 +0530

    bug fixes

--- api/ai.js
+++ api/ai.js
@@ -2,7 +2,7 @@ import {
   GoogleGenerativeAI,
 } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.VITE_GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
   model: "gemini-2.0-flash-exp",
 });
